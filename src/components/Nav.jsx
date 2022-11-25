f91082c5b496221439cfce2fecf9047e57a29715
mely-0 <3145756813mela@gmail.com>
2022-11-25 15:59:15 -0500

    first commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { useState } from 'react';
 import './nav.css';
 
 export const Nav = () => {
+    const[nombre,setNombre]=useState("")
+    const onSubmit=(e)=>{
+    e.preventDefault()
+    setNombre( e.target.user.value);
+    e.target.user.value="";
+    };
+    
     return (
+        <div>
         <div className="Nav">
             <Link to="./BillGates">Bill Gates</Link>
             <Link to="./AdaLovelace">AdaLovelace</Link>
@@ -11,6 +20,14 @@ export const Nav = () => {
             <Link to="./KenThompson">KenThompson</Link>
             <Link to="./LinusTorvalds">LinusTorvalds</Link>
         </div>
+            <div className='contenedor-pro'>
+            <form className='formula' onSubmit={onSubmit}>
+            <input className='nombre' type="text" placeholder='Introduxca su nombre' name='user'  />
+            <button className='boton'>Click</button>
+            </form>
+            <h4 className='texto'>bienvenido {nombre} </h4>
+        </div>
+    </div>
     );
 }
 export default Nav;
